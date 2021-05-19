b402763d76838a2f70e63c72ba31ba766c07d49e
Yaroslav Vavilin <yarikvavilin@gmail.com>
2021-05-19 14:56:56 +0500

    add switch background movies(switch to last movie)

--- src/index.js
+++ src/index.js
@@ -1,94 +1,4 @@
-var state = new Object({
-  detective: [
-    {
-      name: 'Достать ножи', countryAndYear: 'США, 2019', genre: 'Детектив, Комедия', rating: '16+', IMDb: 'IMDb: 7.9', kinopoisk: 'Кинопоиск: 8.0', 'pathImg': './assets/detective/knifes.jpeg'
-    },
-    {
-      name: 'Шестое чувство', countryAndYear: 'США, 1999',genre: 'Детектив, Фэнтези', rating: '12+', IMDb: 'IMDb: 8.1', kinopoisk: 'Кинопоиск: 8.2', 'pathImg': './assets/detective/sixth-sense.jpeg'
-    },
-    {
-      name: 'Поиск', countryAndYear: 'Россия, США, 1999', genre: 'Детектив, Триллер', rating: '16+', IMDb: 'IMDb: 8.20', kinopoisk: 'Кинопоиск: 8.5', 'pathImg': './assets/detective/searching.jpeg'
-    }
-  ],
-  fantasy: [
-    {
-      name: 'Начало', countryAndYear: 'США, Великобритания, 2010', genre: 'фантастика, боевик', rating: '12+', IMDb: 'IMDb: 8.80', kinopoisk: 'Кинопоиск: 8.7', 'pathImg': './assets/fantasy/inception.jpeg'
-    },
-    {
-      name: 'Интерстеллар', countryAndYear: 'США, Великобритания, 2014', genre: 'фантастика, драма', rating: '12+', IMDb: 'IMDb: 8.6', kinopoisk: 'Кинопоиск: 8.6', 'pathImg': './assets/fantasy/inters.jpg'
-    },
-    {
-      name: 'Мстители: Война бесконечности', countryAndYear: 'США, 2018', genre: 'фантастика, боевик', rating: '16+', IMDb: 'IMDb: 8.4', kinopoisk: 'Кинопоиск: 7.9', 'pathImg': './assets/fantasy/avengers.jpeg'
-    }
-  ],
-  actionMovie: [
-    {
-      name: 'Леон', countryAndYear: 'Франция, США, 1994', genre: 'боевик, триллер', rating: '16+', IMDb: 'IMDb: 8.50', kinopoisk: 'Кинопоиск: 8.7', 'pathImg': './assets/actionMovie/leon.jpeg'
-    },
-    {
-      name: 'Гладиатор', countryAndYear: 'США, Великобритания, 2000', genre: 'боевик, история', rating: '16+', IMDb: 'IMDb: 8.50', kinopoisk: 'Кинопоиск: 8.6', 'pathImg': './assets/actionMovie/gladiator.jpeg'
-    },
-    {
-      name: 'Карты, деньги, два ствола', countryAndYear: 'Великобритания, 1998', genre: 'боевик, комедия', rating: '18+', IMDb: 'IMDb: 8.20', kinopoisk: 'Кинопоиск: 8.5', 'pathImg': './assets/actionMovie/two-guns.jpeg'
-    }
-  ],
-  children: [
-    {
-      name: 'Бамблби', countryAndYear: 'Китай, США, 2018', genre: 'фантастика, боевик', rating: '6+', IMDb: 'IMDb: 6.80', kinopoisk: 'Кинопоиск: 6.5', 'pathImg': './assets/children/bumb.jpeg'
-    },
-    {
-      name: 'Аладдин', countryAndYear: 'США, Великобритания,  2019', genre: 'мюзикл, фэнтези', rating: '6+', IMDb: 'IMDb: 6.90', kinopoisk: 'Кинопоиск: 7.2', 'pathImg': './assets/children/alladin.jpeg'
-    },
-    {
-      name: 'Приключения Паддингтона', countryAndYear: 'Великобритания, Франция, 2019', genre: 'комедия, семейный',rating: '6+', IMDb: 'IMDb: 7.20', kinopoisk: 'Кинопоиск: 7.2', 'pathImg': './assets/children/paddington.jpeg'
-    }
-  ],
-  comedy: [
-    {
-      name: 'Залечь на дно в Брюгге', countryAndYear: 'Великобритания, США, 2007', genre: 'комедия, криминал', rating: '18+', IMDb: 'IMDb: 7.90', kinopoisk: 'Кинопоиск: 7.7', 'pathImg': './assets/comedy/brugge.jpeg'
-    },
-    {
-      name: 'Большой Лебовски', countryAndYear: 'США, Великобритания, 1998', genre: 'комедия, криминал', rating: '18+', IMDb: 'IMDb: 8.10', kinopoisk: 'Кинопоиск: 7.7', 'pathImg': './assets/comedy/lebovs.jpeg'
-    },
-    {
-      name: 'Впритык', countryAndYear: 'США, 2010', genre: 'комедия', rating: '16+', IMDb: 'IMDb: 6.50', kinopoisk: 'Кинопоиск: 7.24', 'pathImg': './assets/comedy/vprituk.jpeg'
-    }
-  ],
-  melodrama: [
-    {
-      name: 'Форрест Гамп', countryAndYear: 'США, 1994', genre: 'драма, мелодрама', rating: '12+', IMDb: 'IMDb: 8.8', kinopoisk: 'Кинопоиск: 8.9', 'pathImg': './assets/melodrama/forest.jpeg'
-    },
-    {
-      name: 'Титаник', countryAndYear: 'США, 1997', genre: 'мелодрама', rating: '12+', IMDb: 'IMDb: 8.8', kinopoisk: 'Кинопоиск: 8.9', 'pathImg': './assets/melodrama/titanic.jpeg'
-    },
-    {
-      name: 'Великий Гэтсби', countryAndYear: 'Австралия, США, 2013', genre: 'драма, мелодрама', rating: '16+', IMDb: 'IMDb: 7.20', kinopoisk: 'Кинопоиск: 7.9', 'pathImg': './assets/melodrama/gatsby.jpeg'
-    }
-  ],
-  adventure: [
-    {
-      name: 'Властелин колец: Братство Кольца', countryAndYear: 'США, Новая Зеландия, 2001', genre: 'фэнтези, приключения', rating: '12+', IMDb: 'IMDb: 8.8', kinopoisk: 'Кинопоиск: 8.58', 'pathImg': './assets/adventure/ring.jpeg'
-    },
-    {
-      name: 'Выживший', countryAndYear: 'США, 2015', genre: 'приключения, вестерн', rating: '18+', IMDb: 'IMDb: 8.0', kinopoisk: 'Кинопоиск: 7.8', 'pathImg': './assets/adventure/survivor.jpeg'
-    },
-    {
-      name: 'Алиса в Стране чудес', countryAndYear: 'США, Великобритания, 2010', genre: 'фэнтези, приключения', rating: '12+', IMDb: 'IMDb: 6.40', kinopoisk: 'Кинопоиск: 7.1', 'pathImg': './assets/adventure/alice.jpeg'
-    }
-  ],
-  sizeGenreContainer: {
-    prev:   0,
-    active: 0,
-    next:   0
-  },
-  activeGenre:        'detective',
-  lastActiveElements: {
-    fromNavToGenres:    null,
-    fromGenresToMovies: null,
-    fromMoviesToGenres: null,
-    fromGenresToNav:    null
-  }
-});
+
 
 var keys = ['ArrowUp','ArrowDown','ArrowLeft', 'ArrowRight'];
 
@@ -111,7 +21,6 @@ var elements = {
   }
 };
 function renderBackgroung(pathImg) {
-  // var backgroundImg = pathImg;
   var back = document.getElementsByClassName('backgroundMovie')[0];
   back.style.background = 'url(' + pathImg + ')';
   back.style['background-repeat'] = 'no-repeat';
@@ -132,7 +41,7 @@ function renderMovie(element) {
   });
 }
 
-function renderSwitchMovie(el) {
+function renderSwitchMovie(el, state) {
   var data = state[state.activeGenre];
   var movieIndex = el.dataset.position;
 
@@ -154,7 +63,7 @@ function renderSwitchMovie(el) {
 }
 
 var router = {
-  routeFromNavToGenres: function(genres, lastActiveElement) {
+  routeFromNavToGenres: function(genres, lastActiveElement, state) {
     elements.moviesSection.style.display = 'flex';
     if (lastActiveElement === null) {
       state.activeGenre = 'detective';
@@ -175,11 +84,14 @@ var router = {
   routeFromGenresToNav: function(lastEl) {
     lastEl.focus();
   },
-  routeFromGenreToMovies: function(movies, lastActiveElement) {
+  routeFromGenreToMovies: function(movies, lastActiveElement, state) {
     if (lastActiveElement === null) {
       movies.children[0].focus();
     } else {
-      lastActiveElement.focus();
+      var lastActivePositon = state.lastActiveMovieOfGenre[state.activeGenre];
+      renderBackgroung(state[state.activeGenre][lastActivePositon].pathImg);
+      var el = document.getElementById(lastActivePositon);
+      el.focus();
     }
   },
   routeFromMovieToGenre: function(lastEl) {
@@ -188,7 +100,7 @@ var router = {
 };
 
 
-function renderNavigationMenu(keyCode, prevEl, nextEl) {
+function renderNavigationMenu(keyCode, prevEl, nextEl, state) {
   if (keyCode === 'ArrowRight') {
     if (nextEl !== null) {
       nextEl.focus();
@@ -203,12 +115,13 @@ function renderNavigationMenu(keyCode, prevEl, nextEl) {
     state.activeState = 'genres';
     state.lastActiveElements.fromNavToGenres = document.activeElement;
     document.activeElement.blur();
-    console.log(state.activeGenre);
-    router.routeFromNavToGenres(elements.genresListElements, state.lastActiveElements.fromGenresToMovies);
+    // console.log(state.activeGenre);
+    router.routeFromNavToGenres(elements.genresListElements, state.lastActiveElements.fromGenresToMovies, state);
   }
 }
 
-function renderGenresList(keyCode, prevEl, nextEl) {
+function renderGenresList(keyCode, prevEl, nextEl, state) {
+
   if (keyCode === 'ArrowRight') {
     if (nextEl !== null) {
       var prop = [
@@ -222,8 +135,11 @@ function renderGenresList(keyCode, prevEl, nextEl) {
 
       var nextGenreName = nextEl.getAttribute('id');
       state.activeGenre = nextGenreName;
-
+      console.log(state.activeGenre);
+      var pathNextGenreBackground = state[state.activeGenre][0].pathImg;
+      // console.log(pathBackgroundNextEl)
       renderMovie(state[nextGenreName]);
+      renderBackgroung(pathNextGenreBackground);
       nextEl.focus();
     }
   }
@@ -239,7 +155,8 @@ function renderGenresList(keyCode, prevEl, nextEl) {
       state.activeGenre = prevGenreName;
 
       renderMovie(state[prevGenreName]);
-
+      var pathPrevGenreBackground = state[state.activeGenre][0].pathImg;
+      renderBackgroung(pathPrevGenreBackground);
       var container2 = prevEl.parentNode;
       container2.style.transform = prop2;
       container2.style.transition = '0.5s';
@@ -250,67 +167,178 @@ function renderGenresList(keyCode, prevEl, nextEl) {
     state.activeState = 'movies';
     state.lastActiveElements.fromGenresToMovies = document.activeElement;
     document.activeElement.blur();
-    console.log(state.lastActiveElements);
-    router.routeFromGenreToMovies(elements.moviesContainer, state.lastActiveElements.fromMoviesToGenres);
+    // console.log(state.lastActiveElements);
+    router.routeFromGenreToMovies(elements.moviesContainer, state.lastActiveElements.fromMoviesToGenres, state);
   }
   if (keyCode === 'ArrowUp') {
     state.lastActiveElements.fromGenresToNav = document.activeElement;
     document.activeElement.blur();
     console.log(state.lastActiveElements);
-    router.routeFromGenresToNav(state.lastActiveElements.fromNavToGenres);
+    router.routeFromGenresToNav(state.lastActiveElements.fromNavToGenres, state);
   }
 }
 
-function renderMoviesList(keyCode, prevEl, nextEl) {
+function renderMoviesList(keyCode, prevEl, nextEl, state) {
+
   if (keyCode === 'ArrowRight') {
     if (nextEl !== null) {
-      renderSwitchMovie(nextEl);
+      renderSwitchMovie(nextEl, state);
+      var pathImgNextEl = state[state.activeGenre][nextEl.dataset.position].pathImg;
+      renderBackgroung(pathImgNextEl);
       nextEl.focus();
     }
   }
   if (keyCode === 'ArrowLeft') {
     if (prevEl !== null) {
-      renderSwitchMovie(prevEl);
+      renderSwitchMovie(prevEl, state);
+      var pathImgPrevtEl = state[state.activeGenre][prevEl.dataset.position].pathImg;
+      renderBackgroung(pathImgPrevtEl);
       prevEl.focus();
     }
   }
   if (keyCode === 'ArrowUp') {
-    console.log(document.activeElement);
     state.lastActiveElements.fromMoviesToGenres = document.activeElement;
+    state.lastActiveMovieOfGenre[state.activeGenre] = document.activeElement.dataset.position;
     document.activeElement.blur();
-    router.routeFromMovieToGenre(state.lastActiveElements.fromGenresToMovies);
+    router.routeFromMovieToGenre(state.lastActiveElements.fromGenresToMovies, state);
   }
 }
 
-document.addEventListener('keydown', function(e) {
-  if (keys.indexOf(e.code) > -1) {
-    e.preventDefault();
-  }
-  if (e.code === 'ArrowDown') {
-    elements.navElements[0].focus();
-  }
+function app() {
+  var state = new Object({
+    detective: [
+      {
+        name: 'Достать ножи', countryAndYear: 'США, 2019', genre: 'Детектив, Комедия', rating: '16+', IMDb: 'IMDb: 7.9', kinopoisk: 'Кинопоиск: 8.0', 'pathImg': './assets/detective/knifes.jpeg'
+      },
+      {
+        name: 'Шестое чувство', countryAndYear: 'США, 1999',genre: 'Детектив, Фэнтези', rating: '12+', IMDb: 'IMDb: 8.1', kinopoisk: 'Кинопоиск: 8.2', 'pathImg': './assets/detective/sixth-sense.jpeg'
+      },
+      {
+        name: 'Поиск', countryAndYear: 'Россия, США, 1999', genre: 'Детектив, Триллер', rating: '16+', IMDb: 'IMDb: 8.20', kinopoisk: 'Кинопоиск: 8.5', 'pathImg': './assets/detective/searching.jpeg'
+      }
+    ],
+    fantasy: [
+      {
+        name: 'Начало', countryAndYear: 'США, Великобритания, 2010', genre: 'фантастика, боевик', rating: '12+', IMDb: 'IMDb: 8.80', kinopoisk: 'Кинопоиск: 8.7', 'pathImg': './assets/fantasy/inception.jpeg'
+      },
+      {
+        name: 'Интерстеллар', countryAndYear: 'США, Великобритания, 2014', genre: 'фантастика, драма', rating: '12+', IMDb: 'IMDb: 8.6', kinopoisk: 'Кинопоиск: 8.6', 'pathImg': './assets/fantasy/inters.jpg'
+      },
+      {
+        name: 'Мстители: Война бесконечности', countryAndYear: 'США, 2018', genre: 'фантастика, боевик', rating: '16+', IMDb: 'IMDb: 8.4', kinopoisk: 'Кинопоиск: 7.9', 'pathImg': './assets/fantasy/avengers.jpeg'
+      }
+    ],
+    actionMovie: [
+      {
+        name: 'Леон', countryAndYear: 'Франция, США, 1994', genre: 'боевик, триллер', rating: '16+', IMDb: 'IMDb: 8.50', kinopoisk: 'Кинопоиск: 8.7', 'pathImg': './assets/actionMovie/leon.jpeg'
+      },
+      {
+        name: 'Гладиатор', countryAndYear: 'США, Великобритания, 2000', genre: 'боевик, история', rating: '16+', IMDb: 'IMDb: 8.50', kinopoisk: 'Кинопоиск: 8.6', 'pathImg': './assets/actionMovie/gladiator.jpeg'
+      },
+      {
+        name: 'Карты, деньги, два ствола', countryAndYear: 'Великобритания, 1998', genre: 'боевик, комедия', rating: '18+', IMDb: 'IMDb: 8.20', kinopoisk: 'Кинопоиск: 8.5', 'pathImg': './assets/actionMovie/two-guns.jpeg'
+      }
+    ],
+    children: [
+      {
+        name: 'Бамблби', countryAndYear: 'Китай, США, 2018', genre: 'фантастика, боевик', rating: '6+', IMDb: 'IMDb: 6.80', kinopoisk: 'Кинопоиск: 6.5', 'pathImg': './assets/children/bumb.jpeg'
+      },
+      {
+        name: 'Аладдин', countryAndYear: 'США, Великобритания,  2019', genre: 'мюзикл, фэнтези', rating: '6+', IMDb: 'IMDb: 6.90', kinopoisk: 'Кинопоиск: 7.2', 'pathImg': './assets/children/alladin.jpeg'
+      },
+      {
+        name: 'Приключения Паддингтона', countryAndYear: 'Великобритания, Франция, 2019', genre: 'комедия, семейный',rating: '6+', IMDb: 'IMDb: 7.20', kinopoisk: 'Кинопоиск: 7.2', 'pathImg': './assets/children/paddington.jpeg'
+      }
+    ],
+    comedy: [
+      {
+        name: 'Залечь на дно в Брюгге', countryAndYear: 'Великобритания, США, 2007', genre: 'комедия, криминал', rating: '18+', IMDb: 'IMDb: 7.90', kinopoisk: 'Кинопоиск: 7.7', 'pathImg': './assets/comedy/brugge.jpeg'
+      },
+      {
+        name: 'Большой Лебовски', countryAndYear: 'США, Великобритания, 1998', genre: 'комедия, криминал', rating: '18+', IMDb: 'IMDb: 8.10', kinopoisk: 'Кинопоиск: 7.7', 'pathImg': './assets/comedy/lebovs.jpeg'
+      },
+      {
+        name: 'Впритык', countryAndYear: 'США, 2010', genre: 'комедия', rating: '16+', IMDb: 'IMDb: 6.50', kinopoisk: 'Кинопоиск: 7.24', 'pathImg': './assets/comedy/vprituk.jpeg'
+      }
+    ],
+    melodrama: [
+      {
+        name: 'Форрест Гамп', countryAndYear: 'США, 1994', genre: 'драма, мелодрама', rating: '12+', IMDb: 'IMDb: 8.8', kinopoisk: 'Кинопоиск: 8.9', 'pathImg': './assets/melodrama/forest.jpeg'
+      },
+      {
+        name: 'Титаник', countryAndYear: 'США, 1997', genre: 'мелодрама', rating: '12+', IMDb: 'IMDb: 8.8', kinopoisk: 'Кинопоиск: 8.9', 'pathImg': './assets/melodrama/titanic.jpeg'
+      },
+      {
+        name: 'Великий Гэтсби', countryAndYear: 'Австралия, США, 2013', genre: 'драма, мелодрама', rating: '16+', IMDb: 'IMDb: 7.20', kinopoisk: 'Кинопоиск: 7.9', 'pathImg': './assets/melodrama/gatsby.jpeg'
+      }
+    ],
+    adventure: [
+      {
+        name: 'Властелин колец: Братство Кольца', countryAndYear: 'США, Новая Зеландия, 2001', genre: 'фэнтези, приключения', rating: '12+', IMDb: 'IMDb: 8.8', kinopoisk: 'Кинопоиск: 8.58', 'pathImg': './assets/adventure/ring.jpeg'
+      },
+      {
+        name: 'Выживший', countryAndYear: 'США, 2015', genre: 'приключения, вестерн', rating: '18+', IMDb: 'IMDb: 8.0', kinopoisk: 'Кинопоиск: 7.8', 'pathImg': './assets/adventure/survivor.jpeg'
+      },
+      {
+        name: 'Алиса в Стране чудес', countryAndYear: 'США, Великобритания, 2010', genre: 'фэнтези, приключения', rating: '12+', IMDb: 'IMDb: 6.40', kinopoisk: 'Кинопоиск: 7.1', 'pathImg': './assets/adventure/alice.jpeg'
+      }
+    ],
+    sizeGenreContainer: {
+      prev:   0,
+      active: 0,
+      next:   0
+    },
+    activeGenre:              'detective',
+    'lastActiveMovieOfGenre': {
+      detective:   0,
+      fantasy:     0,
+      actionMovie: 0,
+      children:    0,
+      comedy:      0,
+      melodrama:   0,
+      adventure:   0
+    },
+    lastActiveElements: {
+      fromNavToGenres:    null,
+      fromGenresToMovies: null,
+      fromMoviesToGenres: null,
+      fromGenresToNav:    null
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (keys.indexOf(e.code) > -1) {
+      e.preventDefault();
+    }
+    if (e.code === 'ArrowDown') {
+      elements.navElements[0].focus();
+    }
 
-  elements.navContainer.addEventListener('keydown', function(e) {
-    e.stopImmediatePropagation();
-    var active = e.target;
-    var prevNavEl = active.previousElementSibling;
-    var nextNavEl = active.nextElementSibling;
-    renderNavigationMenu(e.code, prevNavEl, nextNavEl);
+    elements.navContainer.addEventListener('keydown', function(e) {
+      e.stopImmediatePropagation();
+      var active = e.target;
+      var prevNavEl = active.previousElementSibling;
+      var nextNavEl = active.nextElementSibling;
+      renderNavigationMenu(e.code, prevNavEl, nextNavEl, state);
+    });
+
+    elements.genresContainer.addEventListener('keydown', function(e) {
+      e.stopImmediatePropagation();
+      var active = e.target;
+      var prevGenreEl = active.previousElementSibling;
+      var nextGenreEl = active.nextElementSibling;
+      renderGenresList(e.code, prevGenreEl, nextGenreEl, state);
+    });
+
+    elements.moviesContainer.addEventListener('keydown', function(e) {
+      e.stopImmediatePropagation();
+      var active = e.target;
+      var prevMovieEl = active.previousElementSibling;
+      var nextMovieEl = active.nextElementSibling;
+      renderMoviesList(e.code, prevMovieEl, nextMovieEl, state);
+    });
   });
 
-  elements.genresContainer.addEventListener('keydown', function(e) {
-    e.stopImmediatePropagation();
-    var active = e.target;
-    var prevGenreEl = active.previousElementSibling;
-    var nextGenreEl = active.nextElementSibling;
-    renderGenresList(e.code, prevGenreEl, nextGenreEl);
-  });
+}
 
-  elements.moviesContainer.addEventListener('keydown', function(e) {
-    e.stopImmediatePropagation();
-    var active = e.target;
-    var prevMovieEl = active.previousElementSibling;
-    var nextMovieEl = active.nextElementSibling;
-    renderMoviesList(e.code, prevMovieEl, nextMovieEl);
-  });
-});
+
+app();
